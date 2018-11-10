6147b0328dac314d48fe9bd938f2c7cbb93831a9
Andrew <milichab@gmail.com>
2018-11-10 15:57:42 -0800

    hash

--- facenet_external/lfw_input.py
+++ facenet_external/lfw_input.py
@@ -87,9 +87,9 @@ def get_image_paths_and_labels(dataset):
         label_name = re.split(r'(\d+)', image_paths_flat[0])[0]
         if label_name[-1] == '_':
             label_name = label_name[:-1]
-        print(label_name)
         digest = hashlib.sha1(label_name.strip().encode()).hexdigest()
         digest_int = int(digest,16)
+        print('label name: {}, label idx = {}'.format(label_name, digest_int))
         labels_flat += [i] * len(dataset[i].image_paths)
     return image_paths_flat, labels_flat
 
