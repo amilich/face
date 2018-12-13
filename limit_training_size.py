641ca12c5f9b0534c82bd2899d6f3640e08ed2bd
Andrew <milichab@gmail.com>
2018-12-13 15:52:11 -0500

    adding limit

--- limit_training_size.py
+++ limit_training_size.py
@@ -31,18 +31,19 @@ def main(argv):
         if folderName.startswith('.'):
             continue
         print(folderName)
-        newSubDir = newDirName + '/' + folderName + '/'
-        print('new dir {}'.format(newSubDir))
-        if not os.path.exists(newSubDir):
-            os.makedirs(newSubDir)
-            os.chmod(newSubDir, 0o777)
-        # count number of items in each dir
         subdir_name = dir_1 + '/' + folderName
         num_files = len([name for name in os.listdir(subdir_name)\
                         if os.path.isfile(os.path.join(subdir_name, name))])
         print('There are {} files in {}'.format(num_files, subdir_name))
         if num_files < MIN_PHOTOS:
             continue
+        
+        newSubDir = newDirName + '/' + folderName + '/'
+        print('new dir {}'.format(newSubDir))
+        if not os.path.exists(newSubDir):
+            os.makedirs(newSubDir)
+            os.chmod(newSubDir, 0o777)
+        # count number of items in each dir
         count = 0
         for filename in os.listdir(subdir_name):
             img_file = dir_1 + '/' + folderName + '/' + filename 
